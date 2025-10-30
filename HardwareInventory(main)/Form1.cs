7b196652909c08675a3b8254533af244509da8e9
Zerkh Jade Genobiagon <zerkhjade@gmail.com>
2025-10-30 23:20:33 +0800

    .

--- HardwareInventory(main)/Form1.cs
+++ HardwareInventory(main)/Form1.cs
@@ -16,5 +16,10 @@ namespace HardwareInventory_main_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              
+        }
     }
 }
