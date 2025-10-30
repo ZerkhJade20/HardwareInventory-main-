0db18edc4b5092206960587166653d2100e387d2
migzCastro501 <kuyabrian501@gmail.com>
2025-10-31 00:45:29 +0800

    zerkh asshole

--- HardwareInventory(main)/FrmAdmin.Designer.cs
+++ HardwareInventory(main)/FrmAdmin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HardwareInventory_main_
 {
-    partial class Form1
+    partial class FrmAdmin
     {
         /// <summary>
         /// Required designer variable.
@@ -29,34 +29,28 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.btnWorker = new System.Windows.Forms.Button();
-            this.btnAdmin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox1);
             this.panel2.Controls.Add(this.btnWorker);
-            this.panel2.Controls.Add(this.btnAdmin);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(247, 36);
+            this.panel2.Location = new System.Drawing.Point(246, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(608, 554);
-            this.panel2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 129);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(267, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "To Your Account To continue";
+            this.panel2.Size = new System.Drawing.Size(608, 564);
+            this.panel2.TabIndex = 3;
             // 
             // btnWorker
             // 
@@ -64,25 +58,12 @@
             this.btnWorker.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnWorker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnWorker.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnWorker.Location = new System.Drawing.Point(117, 413);
+            this.btnWorker.Location = new System.Drawing.Point(387, 483);
             this.btnWorker.Name = "btnWorker";
-            this.btnWorker.Size = new System.Drawing.Size(378, 51);
+            this.btnWorker.Size = new System.Drawing.Size(195, 51);
             this.btnWorker.TabIndex = 2;
-            this.btnWorker.Text = "Worker";
+            this.btnWorker.Text = "Log In";
             this.btnWorker.UseVisualStyleBackColor = false;
-            // 
-            // btnAdmin
-            // 
-            this.btnAdmin.BackColor = System.Drawing.Color.SlateGray;
-            this.btnAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdmin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnAdmin.Location = new System.Drawing.Point(117, 325);
-            this.btnAdmin.Name = "btnAdmin";
-            this.btnAdmin.Size = new System.Drawing.Size(378, 47);
-            this.btnAdmin.TabIndex = 1;
-            this.btnAdmin.Text = "Admin";
-            this.btnAdmin.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
@@ -90,26 +71,59 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(16, 40);
+            this.label1.Location = new System.Drawing.Point(195, 137);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 69);
+            this.label1.Size = new System.Drawing.Size(221, 69);
             this.label1.TabIndex = 0;
-            this.label1.Text = "LogIn";
+            this.label1.Text = "Admin ";
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 293);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 36);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 362);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 36);
+            this.textBox2.TabIndex = 7;
+            // 
+            // FrmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Peru;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::HardwareInventory_main_.Properties.Resources.futuristic_abstract_dark_red_and_white_papercut_overlap_on_bright_white_color_geometric_design_modern_overlap_papercut_background_vector;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1119, 638);
+            this.ClientSize = new System.Drawing.Size(1101, 591);
             this.Controls.Add(this.panel2);
-            this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "FrmAdmin";
+            this.Text = "FrmAdmin";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -119,10 +133,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button btnWorker;
-        private System.Windows.Forms.Button btnAdmin;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
-
